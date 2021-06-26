45f95bbb022c81dc10a2e1dce4350f609f7721c6
Pragya Kumari <pragyakumari004@gmail.com>
2021-06-26 21:08:39 +0530

    changing alignment

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,14 +10,14 @@ function Contact() {
 
     return (
             <div style={{backgroundColor:"white"}} id={'contacts'}>
-                <Container style={{color:"grey"}} fluid className="p-4 d-flex align-items-center flex-column justify-content-center">
+                <Container style={{color:"grey"}} className="w-75 d-flex flex-column justify-content-center footerContainer">
                     <div><h5 className="mt-4">Let's talk</h5>
                     <hr style={{backgroundColor:"grey", width:"80%",float:"left"}} /></div>
-                    <br></br><br></br>
-                    <Container className="d-flex w-75 mb-4 align-items-center justify-content-around">
-                        <div className="mb-4 mt-4"><img src={githublogo} className="mr-4" style={{height:25,width:25}}/><a className="contactlink" href="https://github.com/pragya004" target="_blank">github</a></div>
-                        <div className="mb-4 mt-4"><img src={linkedinlogo} className="mr-4" style={{height:25,width:25}}/><a className="contactlink" href="https://www.linkedin.com/in/pragya-kumari-72979a141" target="_blank">linkedin</a></div>
-                        <div  className="mb-4 mt-4"><img src={gmaillogo} className="mr-4" style={{height:25,width:25}}/><a className="contactlink" href="https://mail.google.com/mail/?view=cm&amp;source=mailto&amp;to=pragyakumari004@gmail.com" target="_blank">email</a></div>
+                    <br></br>
+                    <Container className="d-flex mb-4 justify-content-between footer">
+                        <div className="mb-4 mt-4 d-flex flex-row justify-content-center"><img src={githublogo} className="mr-2" style={{height:25,width:25}}/><a className="contactlink" href="https://github.com/pragya004" target="_blank">github</a></div>
+                        <div className="mb-4 mt-4 d-flex flex-row justify-content-center"><img src={linkedinlogo} className="mr-2" style={{height:25,width:25}}/><a className="contactlink" href="https://www.linkedin.com/in/pragya-kumari-72979a141" target="_blank">linkedin</a></div>
+                        <div  className="mb-4 mt-4 d-flex flex-row justify-content-center"><img src={gmaillogo} className="mr-2" style={{height:25,width:25}}/><a className="contactlink" href="https://mail.google.com/mail/?view=cm&amp;source=mailto&amp;to=pragyakumari004@gmail.com" target="_blank">email</a></div>
                     </Container>
                 </Container>
             </div>
